4ff484f01a06f7beba0ab40911814045372b2d01
Jan <jazevo@web.de>
2021-06-05 14:16:21 +0200

    replace fusioncharts with apexcharts for better multicharts

--- apps/large-trading-api-frontend/src/app/candlestick-chart/candlestick-chart.component.ts
+++ apps/large-trading-api-frontend/src/app/candlestick-chart/candlestick-chart.component.ts
@@ -1,107 +1,43 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-
-declare const FusionCharts: any;
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import ApexCharts from 'apexcharts/dist/apexcharts.common.js';
 
 @Component({
   selector: 'candlestick-chart',
   templateUrl: './candlestick-chart.component.html',
   styleUrls: ['./candlestick-chart.component.scss']
 })
-export class CandlestickChartComponent implements OnInit {
-  @ViewChild('chartContainer')
-  public chartContainer: any;
-
-  public type = 'timeseries';
-  public width = '100%';
-  public height = '400';
-  public dataSource: any;
+export class CandlestickChartComponent implements AfterViewInit, OnInit {
+  @ViewChild('apexChart')
+  public apexChart: ElementRef;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.initData();
-
-    new FusionCharts({
-      type: 'timeseries',
-      renderAt: 'chart-container',
-      width: '100%',
-      height: '400',
-      dataSource: this.dataSource
-    }).render();
   }
 
-  private initData(): void {
-    this.dataSource = {
+  ngAfterViewInit(): void {
+    console.log(this.apexChart);
+    this.initChart();
+  }
+
+  initChart() {
+    var options = {
       chart: {
-        theme: 'candy'
+        type: 'bar'
       },
-      caption: {
-        text: 'Apple Inc. Stock Price'
-      },
-      subcaption: {
-        text: 'Stock prices from January 1980 - November 2011'
-      },
-      yaxis: [
+      series: [
         {
-          plot: {
-            value: {
-              open: 'Open',
-              high: 'High',
-              low: 'Low',
-              close: 'Close'
-            },
-            type: 'candlestick'
-          },
-          format: {
-            prefix: '$'
-          },
-          title: 'Stock Value'
+          name: 'sales',
+          data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
         }
-      ]
-    };
-
-    const data = [
-      [
-        '06/05/2021, 12:29:00 AM',
-        0.513393,
-        0.515625,
-        0.513393,
-        0.513393,
-        117258400
       ],
-      [
-        '06/05/2021, 12:30:00 AM',
-        0.488839,
-        0.488839,
-        0.486607,
-        0.486607,
-        43971200
-      ]
-    ];
+      xaxis: {
+        categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+      }
+    }
 
-    const schema = [{
-      name: 'Date',
-      type: 'date',
-      format: '%m/%d/%Y, %I:%M:%S %p'
-    }, {
-      name: 'Open',
-      type: 'number'
-    }, {
-      name: 'High',
-      type: 'number'
-    }, {
-      name: 'Low',
-      type: 'number'
-    }, {
-      name: 'Close',
-      type: 'number'
-    }, {
-      name: 'Volume',
-      type: 'number'
-    }]
-
-    const dataStore = new FusionCharts.DataStore();
-    this.dataSource.data = dataStore.createDataTable(data, schema);
+    const chart = new ApexCharts(this.apexChart.nativeElement, options);
+    chart.render();
   }
 
 }
