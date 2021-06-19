9dfb59dfe398d3d7d0bc2e5635301eeb4ef51364
Jan <jazevo@web.de>
2021-06-19 14:05:01 +0200

    feat (macd controller) switch strat

--- src/controllers/algorithms/macd-controller.ts
+++ src/controllers/algorithms/macd-controller.ts
@@ -9,11 +9,18 @@ export default class MacdController {
 
   public setSignals(klines: Array<any>, fast: string, slow: string, signal: string): Array<any> {
     const histogram = this.indicatorsController.macd(klines, fast, slow, signal);
+    const klinesWithHistogram = klines.slice(klines.length - histogram.length, klines.length);
 
     let lastHistogram: number;
     let lastMove: string;
-
-    const klinesWithHistogram = klines.slice(klines.length - histogram.length, klines.length);
+    let sumHighs = 0;
+    let peakHigh = 0;
+    let numberHighs = 0;
+    let sumLows = 0;
+    let peakLow = 0;
+    let numberLows = 0;
+    let positionOpen = false;
+    let positionOpenType: string;
 
     klinesWithHistogram.forEach((kline, index) => {
       const h = histogram[index].histogram;
@@ -30,9 +37,38 @@ export default class MacdController {
       const move = h - lastHistogram > 0 ? 'up' : 'down';
       const momentumSwitch = move !== lastMove;
 
+      // buy when macd h. is decreasing at high value or increasing at low value, sell when macd h. hits 0
       if (momentumSwitch) {
-        const entry = move === 'up' ? 'BUY' : 'SELL';
-        kline.push(entry);
+        if (!positionOpen) {
+          if (move === 'down' && h > 0) {
+            sumHighs += h;
+            numberHighs++;
+            peakHigh = h > peakHigh ? h : peakHigh;
+            const averageHigh = sumHighs / numberHighs;
+
+            if (h > (averageHigh + peakHigh) / 2) {
+              kline.push('SELL');
+              positionOpen = true;
+              positionOpenType = 'SELL';
+            }
+          } else if (move === 'up' && h < 0) {
+            sumLows += h;
+            numberLows++;
+            peakLow = h > peakLow ? h : peakLow;
+            const averageLow = sumLows / numberLows;
+
+            if (h < (averageLow + peakLow) / 2) {
+              kline.push('BUY');
+              positionOpen = true;
+              positionOpenType = 'BUY';
+            }
+          }
+        } else {
+          if ((positionOpenType === 'SELL' && h < 0) || (positionOpenType === 'BUY' && h > 0)) {
+            kline.push('CLOSE');
+            positionOpen = false;
+          }
+        }
       }
 
       lastHistogram = h;
