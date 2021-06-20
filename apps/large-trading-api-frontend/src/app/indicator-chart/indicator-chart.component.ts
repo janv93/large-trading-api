b74c0032395a65cbb33cc7c33858df416b1c60bb
jan2015-PC\jan2015 <jazevo@web.de>
2021-06-21 00:31:53 +0200

    put rsi grid in front

--- apps/large-trading-api-frontend/src/app/indicator-chart/indicator-chart.component.ts
+++ apps/large-trading-api-frontend/src/app/indicator-chart/indicator-chart.component.ts
@@ -52,7 +52,8 @@ export class IndicatorChartComponent implements AfterViewInit {
         }
       },
       grid: {
-        borderColor: '#000'
+        borderColor: '#000',
+        position: 'front'
       },
       tooltip: {
         x: {
