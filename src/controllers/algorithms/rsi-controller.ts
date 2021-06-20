dda9f9ab471ee51c5c9f7480e0227b9fb1180686
jan2015-PC\jan2015 <jazevo@web.de>
2021-06-21 00:28:44 +0200

    fix (rsi controller) assign last signal

--- src/controllers/algorithms/rsi-controller.ts
+++ src/controllers/algorithms/rsi-controller.ts
@@ -10,6 +10,12 @@ export default class RsiController {
   public setSignals(klines: Array<any>, length: number): Array<any> {
     const rsi = this.indicatorsController.rsi(klines, length);
     const klinesWithRsi = klines.slice(-rsi.length);
+    this.overBoughtOverSold(klinesWithRsi, rsi);
+
+    return klines;
+  }
+
+  private overBoughtOverSold(klines: Array<any>, rsi: Array<any>): Array<any> {
     const rsiThresholdHigh = 70;
     const rsiThresholdLow = 30;
     const buySignal = 'BUY';
@@ -17,27 +23,35 @@ export default class RsiController {
 
     let lastSignal: string;
 
-    klinesWithRsi.forEach((kline, index) => {
+    klines.forEach((kline, index) => {
       const r = rsi[index].rsi;
 
       if (lastSignal === buySignal) {
         if (r > rsiThresholdHigh) {
           kline.push(sellSignal);
+          lastSignal = sellSignal;
         }
       } else if (lastSignal === sellSignal) {
         if (r < rsiThresholdLow) {
           kline.push(buySignal);
+          lastSignal = buySignal;
         }
       } else {
         if (r > rsiThresholdHigh) {
           kline.push(sellSignal);
+          lastSignal = sellSignal;
         } else if (r < rsiThresholdLow) {
           kline.push(buySignal);
+          lastSignal = buySignal;
         }
       }
     });
 
     return klines;
+  }
+
+  private rsi50(klines: Array<any>, rsi: Array<any>) {
+
   }
 
 }
