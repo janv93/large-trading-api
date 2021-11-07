54ab255fd4d930af8091852d2e21427481e00f84
Jan <jazevo@web.de>
2021-11-07 17:20:19 +0100

    feat (tensorflow) add dumb example for testing, update readme

--- README.md
+++ README.md
@@ -17,6 +17,11 @@ Frontend: [AngularChartVisualizer](https://github.com/janv93/AngularChartVisuali
 - [x] Find profitable and linear/consistent algorithm on 100k+ / max timeframes (Currently: EMA 80 on BTC 1h, linear, 320% on max time span)
 - [x] When profitable: Start forward test with Binance
 
-### TBD:
+### WIP:
 
-- [ ] Implement Machine Learning: Linear regression on indicators, LSTM on closes
+- [ ] Tensorflow network for price prediction.
+
+
+## Note:
+
+- Install software requiements for tensorflow: https://www.tensorflow.org/install/gpu. As of 11/7/21 you can use the latest version (it states CUDA 11.2 and cuDNN 8.1.0 but latest is tested and works, GPU gets registered "Created device /job:localhost/replica:0/task:0/device:GPU:0 with 8963 MB memory:  -> device: 0, name: NVIDIA GeForce RTX 2080 Ti")
---
+++ src/controllers/algorithms/ai/tensorflow-controller.ts
@@ -0,0 +1,60 @@
+import BaseController from '../../base-controller';
+import * as tf from '@tensorflow/tfjs-node-gpu';
+
+export default class TensorflowController extends BaseController {
+  constructor() {
+    super();
+    this.run();
+  }
+
+  run() {
+    const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    const dataX: Array<any> = []
+    const dataY: Array<any> = [];
+    const dataZ: Array<any> = [];
+
+    dataZ.push([[8]],[[7.3]],[[9.6]]);
+
+    // Creating the data
+    for (let i = 0; i < alphabet.length - 1; i++) {
+      dataX.push([[i+1]]);
+
+      // One-hot-encoding the output values
+      let arr = new Array(alphabet.length).fill(0)
+      arr[alphabet.indexOf(alphabet.charAt(i + 1))] = 1;
+      dataY.push(arr);
+    }
+
+    // Transforming the data to tensors
+    const x = tf.tensor(dataX);
+    const y = tf.tensor(dataY);
+    const z = tf.tensor(dataZ);
+
+    // Printing the tensors
+    z.print()
+
+    // Creating the RNN Model
+    const model = tf.sequential();
+    model.add(tf.layers.lstm({ units: 48, inputShape: [1, 1] }))
+    model.add(tf.layers.dense({ units: 48, activation: 'relu' }));
+    model.add(tf.layers.dense({ units: 48, activation: 'relu' }));
+    model.add(tf.layers.dense({ units: alphabet.length, activation: 'softmax' }));
+
+    // Compiling the model
+    model.compile({
+      optimizer: 'adam',
+      loss: 'categoricalCrossentropy',
+      metrics: ['accuracy']
+    });
+
+    // Fitting the model
+    model.fit(x, y, {
+      batchSize: alphabet.length,
+      epochs: 200
+    }).then((history) => {
+      // printing loss and predictions
+      console.log((model.predict(z) as any).argMax(1).dataSync())
+    });
+  }
+}
