746acf5c7abdf770163d75587bf1e56ef5882c86
Jan <jazevo@web.de>
2021-11-27 04:53:57 +0100

    feat (tensorflow) refactor, add price diff model

--- src/controllers/algorithms/ai/tensorflow-controller.ts
+++ src/controllers/algorithms/ai/tensorflow-controller.ts
@@ -74,13 +74,20 @@ export default class TensorflowController extends BaseController {
     return klines;
   }
 
+  /**
+   * train model on inputs and outputs as price
+   */
   private trainModelPriceToPrice(klines: Array<BinanceKucoinKline>) {
-    const samples = this.createTrainingDataPriceToPrice(klines);
+    const inputCount = 5;
+    const outputCount = 1;
+    const samples = this.createTrainingDataPriceToPrice(klines, inputCount, outputCount);
+
+    // create inputs and outputs
     const dataX: Array<any> = samples.map(sample => sample.inputs);
     const dataY: Array<any> = samples.map(sample => sample.outputs);
     const dataTestX: Array<any> = dataX.slice(-10);
 
-    // Transforming the data to tensors
+    // transforming the data to tensors
     const x = tf.tensor(dataX);
     const y = tf.tensor(dataY);
     const testX = tf.tensor(dataTestX);
@@ -88,22 +95,21 @@ export default class TensorflowController extends BaseController {
     x.print();
     y.print();
 
-    const outputUnits = 1;
-    const act = undefined;
+    const activation = undefined;
 
-    // Creating the Model
+    // creating the Model
     const model = tf.sequential();
-    model.add(tf.layers.dense({ units: 100, inputShape: [5], activation: act }));
-    model.add(tf.layers.dense({ units: outputUnits, activation: act }));
+    model.add(tf.layers.dense({ units: 100, inputShape: [inputCount], activation }));
+    model.add(tf.layers.dense({ units: outputCount, activation }));
 
-    // Compiling the model
+    // compiling the model
     model.compile({
       optimizer: tf.train.adam(),
       loss: tf.losses.meanSquaredError,
       metrics: [tf.metrics.meanAbsoluteError]
     });
 
-    // Fitting the model
+    // fitting the model
     model.fit(x, y, {
       batchSize: 100,
       epochs: 100,
@@ -118,28 +124,99 @@ export default class TensorflowController extends BaseController {
       const predictions = (model.predict(testX) as any).dataSync();
       testX.print();
       console.log(predictions);
-      this.plotlyController.plotPredictions(dataTestX, predictions, outputUnits);
+      this.plotlyController.plotPredictions(dataTestX, predictions, outputCount);
     });
   }
 
+  /**
+   * train model on inputs and outputs as price diff to previous kline
+   */
   private trainModelPriceDiffToPriceDiff(klines: Array<BinanceKucoinKline>) {
+    const inputCount = 5;
+    const outputCount = 1;
+    const samples = this.createTrainingDataPriceDiffToPriceDiff(klines, 5, 1);
 
+    // create inputs and outputs
+    const dataX: Array<any> = samples.map(sample => sample.inputs);
+    const dataY: Array<any> = samples.map(sample => sample.outputs);
+    const dataTestX: Array<any> = dataX.slice(-10);
+
+    // transforming the data to tensors
+    const x = tf.tensor(dataX);
+    const y = tf.tensor(dataY);
+    const testX = tf.tensor(dataTestX);
+
+    x.print();
+    y.print();
+
+    const activation = undefined;
+
+    // creating the Model
+    const model = tf.sequential();
+    model.add(tf.layers.dense({ units: 100, inputShape: [inputCount], activation }));
+    model.add(tf.layers.dense({ units: outputCount, activation }));
+
+    // compiling the model
+    model.compile({
+      optimizer: tf.train.adam(),
+      loss: tf.losses.meanSquaredError,
+      metrics: [tf.metrics.meanAbsoluteError]
+    });
+
+    // fitting the model
+    model.fit(x, y, {
+      batchSize: 100,
+      epochs: 100,
+      validationSplit: 0.9,
+      callbacks: tf.node.tensorBoard('log')
+    }).then((history) => {
+      console.log();
+      console.log('### Training finished ###');
+      console.log();
+
+      // printing loss and predictions
+      const predictions = (model.predict(testX) as any).dataSync();
+      testX.print();
+      console.log(predictions);
+      // this.plotlyController.plotPredictions(dataTestX, predictions, outputCount);
+    });
   }
 
   /**
    * create a training data set with inputs and outputs
    */
-  private createTrainingDataPriceToPrice(klines: Array<BinanceKucoinKline>): Array<any> {
+  private createTrainingDataPriceToPrice(klines: Array<BinanceKucoinKline>, inputCount: number, outputCount: number): Array<any> {
     const closes = klines.map(kline => kline.prices.close);
     const normalizedCloses = this.normalize(closes);
-    const trendLength = 1;
-    const trainingLength = 5;
-    const sampleLength = trainingLength + trendLength;
-    const samples: Array<any> = [];
 
-    for (let i = 0; i < normalizedCloses.length - sampleLength; i++) {
-      const inputs = normalizedCloses.slice(i, i + trainingLength);
-      const outputs = normalizedCloses.slice(i + trainingLength, i + trainingLength + trendLength);
+    return this.createInputsOutputs(normalizedCloses, inputCount, outputCount);
+  }
+
+  /**
+   * create a training data set with inputs and outputs as price diff to previous
+   */
+  private createTrainingDataPriceDiffToPriceDiff(klines: Array<BinanceKucoinKline>, inputCount: number, outputCount: number) {
+    const closes = klines.map(kline => kline.prices.close);
+
+    const diffs = closes.slice(-(closes.length - 1)).map((close, i) => {
+      const previousClose = closes[i];
+      const diff = close - previousClose;
+      return diff / previousClose;
+    });
+
+    return this.createInputsOutputs(diffs, inputCount, outputCount);
+  }
+
+  /**
+   * create samples from sequences in values
+   */
+  private createInputsOutputs(values: Array<number>, inputLength: number, outputLength: number): Array<any> {
+    const samples: Array<any> = [];
+    const totalLength = inputLength + outputLength;
+
+    for (let i = 0; i < values.length - totalLength; i++) {
+      const inputs = values.slice(i, i + inputLength);
+      const outputs = values.slice(i + inputLength, i + inputLength + outputLength);
       const sample = { inputs, outputs };
       samples.push(sample);
     }
