d3c9331b794f5e50268154f2d5b823df8507d1f1
Jan <jazevo@web.de>
2022-04-15 23:12:24 +0200

    feat (profit chart) improvements

--- apps/large-trading-api-frontend/src/app/app.module.ts
+++ apps/large-trading-api-frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -13,6 +14,7 @@ import { IndicatorChartModule } from './indicator-chart/indicator-chart.module';
   imports: [
     BrowserModule,
     CandlestickChartModule,
+    CommonModule,
     ProfitChartModule,
     IndicatorChartModule
   ],
--- apps/large-trading-api-frontend/src/app/profit-chart/profit-chart.module.ts
+++ apps/large-trading-api-frontend/src/app/profit-chart/profit-chart.module.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ProfitChartComponent } from './profit-chart.component';
 import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
+    CommonModule,
     HttpClientModule
   ],
   exports: [
