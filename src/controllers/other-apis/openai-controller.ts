ef297f6e8603d7b657c05f25950c4d5147bd5524
Jan <jazevo@web.de>
2023-03-11 16:07:33 +0100

    feat (openai) use chat completion api as it vastly outperforms normal completion for same cost

--- src/controllers/other-apis/openai-controller.ts
+++ src/controllers/other-apis/openai-controller.ts
@@ -33,7 +33,7 @@ export default class OpenAi extends BaseController {
     if (dbSentiment) { // in database
       return { symbol: symbol.symbol, originalSymbol: symbol.originalSymbol, sentiment: dbSentiment };
     } else {  // not in database, make call
-      const sentiment = await this.postCompletion(tweet, symbol.originalSymbol);
+      const sentiment = await this.postCompletionChat(tweet, symbol.originalSymbol);
       return { symbol: symbol.symbol, originalSymbol: symbol.originalSymbol, sentiment };
     }
   }
@@ -41,6 +41,7 @@ export default class OpenAi extends BaseController {
   // https://platform.openai.com/docs/api-reference/completions
   public async postCompletion(tweet: Tweet, symbol: string): Promise<number> {
     const url = this.baseUrl + '/completions';
+
     const body = {
       model: this.model, // https://platform.openai.com/docs/models/gpt-3
       prompt: this.createSentimentPromptScale(tweet.text, symbol), // single string or array of strings
@@ -54,7 +55,40 @@ export default class OpenAi extends BaseController {
 
     try {
       const res = await axios.post(url, body, { headers: this.headers });
-      const sentiment = res.data.choices.map(c => c.text)[0];
+      const sentiment = res.data.choices[0].text;
+      const numbers = sentiment.match(/\d+/g);
+      const sentimentNumber = numbers?.length && numbers?.length === 1 ? parseInt(numbers[0]) : -1;
+      const sentimentInRange = 0 < sentimentNumber && sentimentNumber < 11 ? sentimentNumber : 5;
+      return sentimentInRange;
+    } catch (err) {
+      this.handleError(err);
+      return 0;
+    }
+  }
+
+  // https://platform.openai.com/docs/api-reference/chat
+  public async postCompletionChat(tweet: Tweet, symbol: string): Promise<number> {
+    const url = this.baseUrl + '/chat/completions';
+
+    const messages = [
+      { role: 'system', content: 'You are an AI language model that specializes in sentiment analysis of crypto twitter.' },
+      { role: 'user', content: this.createSentimentPromptScale(tweet.text, symbol) },
+    ]
+
+    const body = {
+      model: 'gpt-3.5-turbo', // https://platform.openai.com/docs/models/gpt-3
+      messages, // single string or array of strings
+      max_tokens: 10, // max response tokens
+      temperature: 0, // randomness, 0 = none, 2 = max
+      top_p: 1, // alternative to temperature, filters output tokens by probability, 0.1 = only top 10% of tokens
+      n: 1, // amount of responses/completions for 1 prompt
+      stream: false, // real time stream
+      presence_penalty: 2 // punishment for repeated tokens
+    };
+
+    try {
+      const res = await axios.post(url, body, { headers: this.headers });
+      const sentiment = res.data.choices[0].message.content;
       const numbers = sentiment.match(/\d+/g);
       const sentimentNumber = numbers?.length && numbers?.length === 1 ? parseInt(numbers[0]) : -1;
       const sentimentInRange = 0 < sentimentNumber && sentimentNumber < 11 ? sentimentNumber : 5;
@@ -84,6 +118,6 @@ The tweet is:\n\n
 Analyze the tweet and return a sentiment for ${symbol}.
 The sentiment is a number from 1 to 10 where 1 is the most bearish and 10 is the most bullish. If a prediction cannot be determined or the sentiment is neutral, return 5.
 Only output the number from 1 to 10.
-Number: `
+Sentiment: `
   }
 }
