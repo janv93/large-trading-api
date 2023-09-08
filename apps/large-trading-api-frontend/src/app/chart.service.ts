70588fa43b131067c42aecddd71d21d0364e120b
Jan <jazevo@web.de>
2023-09-08 22:22:14 +0200

    ++

--- apps/large-trading-api-frontend/src/app/chart.service.ts
+++ apps/large-trading-api-frontend/src/app/chart.service.ts
@@ -7,13 +7,17 @@ import { environment } from '../environments/environment';
 })
 export class ChartService {
   public klinesSubject = new Subject();
+  public baseUrl = 'http://127.0.0.1:3000';
+  public isInvestmentStrategy: boolean;
+  public twitterUser = environment.twitterUser;
+
   public exchange = 'alpaca'; // binance, alpaca or kucoin; binance: spot - BTCUSDT, kucoin: futures - XBTUSDTM, alpaca: SPY
   public strategy = 'ema';
   public symbol = 'SPY';
   public timeframe = '1h';  // 1m, 5m, 15m, 1h... 1d...
   public times = 100;  // 1 = 1000 timeframes
-  public isInvestmentStrategy: boolean;
   public commission = 0.04;
+
   public rsiLength = 7;
   public emaPeriodOpen = 80;
   public emaPeriodClose = 80;
@@ -21,8 +25,6 @@ export class ChartService {
   public bbPeriod = 21;
   public momentumStreak = 5;
   public martingaleThreshold = 0.01;
-  public twitterUser = environment.twitterUser;
-  public baseUrl = 'http://127.0.0.1:3000';
 
   constructor() {
     this.checkIsInvestmentStrategy();
