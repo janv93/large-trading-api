4e739b52f06715674d81afefdb6e52197373d660
Jan <jazevo@web.de>
2023-09-09 16:33:02 +0200

    ++

--- src/controllers/algorithms/multi-ticker.ts
+++ src/controllers/algorithms/multi-ticker.ts
@@ -4,9 +4,6 @@ import Nasdaq from '../other-apis/nasdaq';
 import Alpaca from '../exchanges/alpaca';
 
 export default class MultiTicker extends Base {
-  private nasdaq = new Nasdaq();
-  private alpaca = new Alpaca();
-
   public async setSignals(klines: Kline[]): Promise<any> {
     console.log(klines[0].symbol)
     return;
