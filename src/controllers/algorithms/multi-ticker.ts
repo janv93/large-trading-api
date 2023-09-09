24749c2ea32b5266692df8f23713433bc3c97608
Jan <jazevo@web.de>
2023-09-09 16:53:07 +0200

    add backtest to multi ticker

--- src/controllers/algorithms/multi-ticker.ts
+++ src/controllers/algorithms/multi-ticker.ts
@@ -1,9 +1,16 @@
 import { Kline } from '../../interfaces';
 import Base from '../base';
+import Ema from './ema';
+import Backtest from './backtest';
 
 export default class MultiTicker extends Base {
+  private ema = new Ema();
+  private backtest = new Backtest();
+
   public async setSignals(klines: Kline[]): Promise<any> {
-    console.log(klines[0].symbol)
+    const klinesWithSignals = this.ema.setSignals(klines, 80, 80);
+    const klinesWithBacktest = this.backtest.calcBacktestPerformance(klinesWithSignals, 0, true);
+    console.log(klines[0].symbol, klinesWithBacktest.at(-1)?.percentProfit);
     return;
   }
 }
