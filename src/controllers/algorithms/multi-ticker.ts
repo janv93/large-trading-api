c366eb64f75d30ae0e78220c3d928b93ab219ce7
Jan <jazevo@web.de>
2023-09-10 23:49:14 +0200

    switch multi ticker algo

--- src/controllers/algorithms/multi-ticker.ts
+++ src/controllers/algorithms/multi-ticker.ts
@@ -1,18 +1,20 @@
 import { Kline } from '../../interfaces';
 import Base from '../base';
-import Ema from './ema';
 import Backtest from './backtest';
+import Ema from './ema';
+import Martingale from './investing/martingale';
 
 export default class MultiTicker extends Base {
   private ema = new Ema();
   private backtest = new Backtest();
+  private martingale = new Martingale();
 
   public setSignals(klines: Kline[]): any {
-    const klinesWithSignals = this.ema.setSignals(klines, 80, 80);
+    const klinesWithSignals = this.martingale.setSignals(klines, 0.1);
     const klinesWithBacktest = this.backtest.calcBacktestPerformance(klinesWithSignals, 0, true);
-    console.log(klines[0].symbol, klinesWithBacktest.at(-1)?.percentProfit);
+    console.log(klines.length, klines[0].symbol, klinesWithBacktest.at(-1)?.percentProfit);
     return;
-    // all time high 20% above old -> scale into short
+    // todo algo: all time high 20% above old -> scale into short
     // only set signal if n klines before now
     // then: only set signal if rsi high
   }
