d4120ee169719fedf45b87cdd914aedf82b44a15
Jan <jazevo@web.de>
2023-09-11 23:40:50 +0200

    reduce alpaca rate limit

--- src/controllers/exchanges/alpaca.ts
+++ src/controllers/exchanges/alpaca.ts
@@ -7,7 +7,7 @@ import stateService from '../state-service';
 export default class Alpaca extends Base {
   private database = database;
   private stateService = stateService;
-  private rateLimitPerMinute = 200;
+  private rateLimitPerMinute = 190;
 
   public async getKlines(symbol: string, timeframe: string, startTime?: number, pageToken?: string): Promise<AlpacaResponse> {
     const baseUrl = 'https://data.alpaca.markets/v2/stocks/' + symbol + '/bars';
