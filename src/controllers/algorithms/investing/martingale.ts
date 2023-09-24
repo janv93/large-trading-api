d657402745f4226a43d3422ccf813b6fbbca2e6f
Jan <jazevo@web.de>
2023-09-24 23:38:45 +0200

    refactor martingale, make it more scalable

--- src/controllers/algorithms/investing/martingale.ts
+++ src/controllers/algorithms/investing/martingale.ts
@@ -1,6 +1,12 @@
 import { Kline } from '../../../interfaces';
 import Base from '../../base';
 
+enum Action {
+  Buy,
+  Close,
+  SetNewHigh,
+  Skip
+}
 
 export default class Martingale extends Base {
   /**
@@ -10,49 +16,70 @@ export default class Martingale extends Base {
    * 4. back to 1.
    */
   public setSignals(klines: Kline[], threshold: number, exitMultiplier: number): Kline[] {
-    const initialClose = klines[0].prices.close;
-    const minDrop = 0.4;
-    let streak = 0;
-    let peak = initialClose;
-    let low = initialClose;
-    let isOpen = false;
+    const state = {
+      threshold,
+      exitMultiplier,
+      minDrop: 0.4,
+      streak: 0,
+      peak: klines[0].prices.close,
+      low: klines[0].prices.close,
+      isOpen: false
+    };
 
     for (const kline of klines) {
-      const close = kline.prices.close;
-      const diffFromLow = (close - low) / low;
-      const diffFromPeak = (peak - close) / peak;
+      const action: Action = this.getAction(kline, state);
 
-      // buy condition
-      if (diffFromPeak > minDrop) {
-        const excess = diffFromPeak - minDrop;
-        const thresholdMultiple = excess / threshold;
-
-        if (thresholdMultiple > streak) {
-          kline.signal = this.buySignal;
-          streak++;
-          kline.amount = Math.pow(2, streak - 1); // start at 2^0
-          isOpen = true;
-          low = close;
-        }
-
-        continue;
-      }
-
-      // close condition
-      if (isOpen && diffFromLow > threshold * exitMultiplier) {
-        kline.signal = this.closeSignal;
-        streak = 0;
-        isOpen = false;
-        peak = close;
-        continue;
-      }
-
-      // new high
-      if (!isOpen && close > peak) {
-        peak = close;
+      switch (action) {
+        case Action.Buy: this.buy(kline, state); break;
+        case Action.Close: this.close(kline, state); break;
+        case Action.SetNewHigh: state.peak = kline.prices.close; break;
+        case Action.Skip:
+        default: break;
       }
     }
 
     return klines;
+  }
+
+  private getAction(kline: Kline, state: any): Action {
+    const close = kline.prices.close;
+    const diffFromLow = (close - state.low) / state.low;
+    const diffFromPeak = (state.peak - close) / state.peak;
+
+    if (diffFromPeak > state.minDrop) {
+      const excess = diffFromPeak - state.minDrop;
+      const thresholdMultiple = excess / state.threshold;
+
+      if (thresholdMultiple > state.streak) {
+        return Action.Buy;
+      } else {
+        return Action.Skip;
+      }
+    }
+
+    if (state.isOpen && diffFromLow > state.threshold * state.exitMultiplier) {
+      return Action.Close;
+    }
+
+    if (!state.isOpen && close > state.peak) {
+      return Action.SetNewHigh;
+    }
+
+    return Action.Skip;
+  }
+
+  private buy(kline: Kline, state: any) {
+    kline.signal = this.buySignal;
+    state.streak++;
+    kline.amount = Math.pow(2, state.streak - 1); // start at 2^0
+    state.isOpen = true;
+    state.low = kline.prices.close;
+  }
+
+  private close(kline: Kline, state: any) {
+    kline.signal = this.closeSignal;
+    state.streak = 0;
+    state.isOpen = false;
+    state.peak = kline.prices.close;
   }
 }
