4dc064fa5237b03e8cc4f42cfbc5ca2418360e54
Jan <jazevo@web.de>
2023-12-09 15:48:29 +0100

    revert last

--- src/controllers/algorithms/investing/dca.ts
+++ src/controllers/algorithms/investing/dca.ts
@@ -1,20 +1,10 @@
 import { Kline } from '../../../interfaces';
 import Base from '../../base';
 
-/**
- * The Dca class extends the Base class and implements a Dollar-Cost Averaging strategy.
- */
+
 export default class Dca extends Base {
-  /**
-   * Sets buy signals on every 10th kline element based on the specified algorithm.
-   *
-   * @param {Kline[]} klines - Array of kline objects representing market data.
-   * @param {string} algorithm - Name of the algorithm used for setting signals.
-   * @returns {Kline[]} The modified array with set signals.
-   */
-  public setSignals(klines: Kline[], algorithm: string): Kline[] {
+  public setSignals(klines: Kline[], algorithm): Kline[] {
     klines.forEach((kline: Kline, i: number) => {
-      // On every 10th kline, set the signal property to the value of this.buySignal.
       if (i % 10 === 0) {
         kline.algorithms[algorithm].signal = this.buySignal;
       }
--- src/controllers/logger.ts
+++ src/controllers/logger.ts
@@ -1,42 +1,68 @@
-import { createLogger, format, transports } from 'winston';
-const { combine, timestamp, label, printf } = format;
+export default class Logger {
+  private logLevel = 'nodb';
 
-// Custom logging format that includes the level, message, and timestamp.
-const logFormat = printf(({ level, message, timestamp }) => {
-  return `${timestamp} [${level.toUpperCase()}]: ${message}`;
-});
+  private colors = {
+    black: '\x1b[30m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+    reset: '\x1b[0m'
+  };
 
-export class Logger {
-  private logger;
+  public log(...args: any[]) {
+    const caller = args.pop();
 
-  constructor(caller: string) {
-    this.logger = createLogger({
-      // Apply custom formatting to all logs.
-      format: combine(
-        label({ label: caller }),
-        timestamp(),
-        logFormat
-      ),
-      transports: [
-        new transports.Console(), // Log to console.
-        // Add other transports here if needed (e.g., file transport).
-      ],
-    });
+    if (this.passesLogLevelCheck(caller)) {
+      console.log(this.getParentLog(caller), ...args);
+    }
   }
 
-  public info(message: string): void {
-    this.logger.info(message);
+  public logErr(...args: any[]) {
+    const caller = args.pop();
+
+    if (this.passesLogLevelCheck(caller)) {
+      console.error(`${this.getParentLog(caller)} ${this.colors.red}ERR${this.colors.reset}`, ...args);
+    }
   }
 
-  public warn(message: string): void {
-    this.logger.warn(message);
+  private getParentLog(caller: object) {
+    const maxLength = 10;
+    const callerName = caller.constructor.name;
+    let color: string;
+
+    switch (callerName) {
+      case 'App':
+        color = this.colors.blue;
+        break;
+      case 'Database':
+        color = this.colors.yellow;
+        break;
+      case 'Alpaca':
+        color = this.colors.cyan;
+        break;
+      case 'Binance':
+      case 'Kucoin':
+      case 'Btse':
+        color = this.colors.magenta;
+        break;
+      default:
+        color = this.colors.reset;
+        break;
+    }
+
+    const paddedName = callerName.toUpperCase().padEnd(maxLength);
+    return `${color}${paddedName}|${this.colors.reset}`;
   }
 
-  public error(message: string): void {
-    this.logger.error(message);
-  }
+  private passesLogLevelCheck(caller: object): boolean {
+    const callerName = caller.constructor.name;
 
-  public debug(message: string): void {
-    this.logger.debug(message);
+    if (this.logLevel === 'nodb') {
+      return callerName !== 'Database';
+    } else return true;
   }
 }
