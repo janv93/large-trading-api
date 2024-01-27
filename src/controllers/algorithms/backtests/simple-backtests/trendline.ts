84757dcc8fc5b8b7800148056febdf4562172081
Jan <jazevo@web.de>
2024-01-27 18:45:01 +0100

    improve pivot points calculation

--- src/controllers/algorithms/backtests/simple-backtests/trendline.ts
+++ src/controllers/algorithms/backtests/simple-backtests/trendline.ts
@@ -6,7 +6,7 @@ export default class Trendline extends Base {
   private charting = new Charting();
 
   public setSignals(klines: Kline[], algorithm: string): Kline[] {
-    const klinesWithPP = this.charting.calcPivotPoints(klines, 10);
+    const klinesWithPP = this.charting.calcPivotPoints(klines, 10, 10);
     klinesWithPP.forEach(k => {
       if (k.metaData?.pivotPoint) {
         k.algorithms[algorithm].signal = Signal.Buy
--- src/controllers/algorithms/patterns/charting.ts
+++ src/controllers/algorithms/patterns/charting.ts
@@ -1,28 +1,30 @@
 import { Kline, PivotPoint } from '../../../interfaces';
 
 export default class Charting {
-  public calcPivotPoints(klines: Kline[], range: number): Kline[] {
+  public calcPivotPoints(klines: Kline[], leftLength: number, rightLength: number): Kline[] {
     klines.forEach((kline: Kline, i: number) => {
       const currentHigh = kline.prices.high;
       const currentLow = kline.prices.low;
   
-      if (klines[i - range] && klines[i + range]) {
-        const prevKlines = klines.slice(0, i);  // all previous klines
-        const nextKlines = klines.slice(i + 1); // all succeeding klines
+      if (klines[i - leftLength] && klines[i + rightLength]) {
+        const leftKlines = klines.slice(0, i);  // all leftious klines
+        const rightKlines = klines.slice(i + 1); // all succeeding klines
   
-        const prevHighRange = prevKlines.slice().reverse().findIndex((prevKline: Kline) => prevKline.prices.high > currentHigh);
-        const nextHighRange = nextKlines.findIndex((nextKline: Kline) => nextKline.prices.high > currentHigh);
-        // how many elements minimum to the left and right have lower highs, -1 means all of them are lower
-        const highRange = Math.min(prevHighRange === -1 ? Infinity : prevHighRange, nextHighRange === -1 ? Infinity : nextHighRange);
+        let leftLengthHigh: number = leftKlines.slice().reverse().findIndex((leftKline: Kline) => leftKline.prices.high > currentHigh);
+        leftLengthHigh = leftLengthHigh === -1 ? Infinity : leftLengthHigh;
+        let rightLengthHigh: number = rightKlines.findIndex((rightKline: Kline) => rightKline.prices.high > currentHigh);
+        rightLengthHigh = rightLengthHigh === -1 ? Infinity : rightLengthHigh;
+        const isHigh = leftLengthHigh >= leftLength && rightLengthHigh >= rightLength
   
-        const prevLowRange = prevKlines.slice().reverse().findIndex((prevKline: Kline) => prevKline.prices.low < currentLow);
-        const nextLowRange = nextKlines.findIndex((nextKline: Kline) => nextKline.prices.low < currentLow);
-        // how many elements minimum to the left and right have higher lows, -1 means all of them are higher
-        const lowRange = Math.min(prevLowRange === -1 ? Infinity : prevLowRange, nextLowRange === -1 ? Infinity : nextLowRange);
+        let leftLenthLow: number = leftKlines.slice().reverse().findIndex((leftKline: Kline) => leftKline.prices.low < currentLow);
+        leftLenthLow = leftLenthLow === -1 ? Infinity : leftLenthLow;
+        let rightLengthLow: number = rightKlines.findIndex((rightKline: Kline) => rightKline.prices.low < currentLow);
+        rightLengthLow = rightLengthLow === -1 ? Infinity : rightLengthLow;
+        const isLow = leftLenthLow >= leftLength && rightLengthLow >= rightLength;
   
-        if (highRange >= range) {
+        if (isHigh) {
           kline.metaData = { pivotPoint: PivotPoint.High };
-        } else if (lowRange >= range) {
+        } else if (isLow) {
           kline.metaData = { pivotPoint: PivotPoint.Low };
         }
       }
