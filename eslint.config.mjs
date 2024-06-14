355ee933869bc6c3d1ef4eacd407f6c91368ff51
Jan <jazevo@web.de>
2024-06-14 11:45:15 +0200

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,12 @@ import tseslint from "typescript-eslint";
 
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions:{ globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    }
+  }
 ];
