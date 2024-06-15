57769acc5b62280ab02361b00299878cfb2260b1
Jan <jazevo@web.de>
2024-06-15 20:30:44 +0200

    do not require CMC api key for multi call

--- src/controllers/other-apis/coinmarketcap.ts
+++ src/controllers/other-apis/coinmarketcap.ts
@@ -25,6 +25,7 @@ export default class Coinmarketcap extends Base {
 
   public async getCryptosByMarketCapRank(rank: number): Promise<string[]> {
     this.log(`Get cryptos by market cap`);
+    if (!process.env.coinmarketcap_api_key) return ['BTC', 'ETH', 'USDT', 'BNB', 'SOL', 'USDC', 'XRP', 'DOGE', 'TON', 'ADA']; // API key should not be required to run /multi
     const url = this.baseUrl + '/cryptocurrency/listings/latest';
     const res = await axios.get(url, { headers: this.headers });
     const top = res.data.data.slice(0, rank);
