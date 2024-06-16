8dd578ed252631ff2342d44291d991abb4f9b80d
Jan <jazevo@web.de>
2024-06-16 14:56:25 +0200

    improve ohlc

--- apps/large-trading-api-frontend/src/app/chart.service.ts
+++ apps/large-trading-api-frontend/src/app/chart.service.ts
@@ -28,7 +28,7 @@ export class ChartService {
   public meanReversionProfitBasedTrailingStopLoss = [0.3, 0.3];
 
   // multi
-  public isMulti = true;
+  public isMulti = false;
   public multiAutoParams = [false, false];  // primary algorithm and optional second algorithm, determines if algo parameters are chosen automatically or from this service
   public multiRank = 15;  // top {rank} tickers of each category. e.g. top 10 of stocks, cryptos etc
 
