1375ad770410252447ee159e4ed5c311f16c79e6
Jan <jazevo@web.de>
2026-04-25 00:42:08 +0200

    fix bug where signals are discarded on 2nd iteration

--- src/controllers/routes.ts
+++ src/controllers/routes.ts
@@ -92,7 +92,7 @@ export default class Routes extends Base {
 
     for (let i = 0; i < algorithms.length; i++) {
       if (autoParams[i]) {
-        tickersWithSignals = this.multiTicker.handleAlgo(allTickers, algorithms[i].algorithm);
+        tickersWithSignals = this.multiTicker.handleAlgo(tickersWithSignals, algorithms[i].algorithm);
       } else {
         tickersWithSignals = await Promise.all(tickersWithSignals.map(async (klines: Kline[]) => {
           const klinesWithSignals: Kline[] = await this.handleAlgo(klines, algorithms[i]);
