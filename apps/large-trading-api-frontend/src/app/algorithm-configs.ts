8795233113e7d315a38fc38121cf6ce1d554adbc
Jan <jazevo@web.de>
2026-05-03 16:32:14 +0200

    ++

--- apps/large-trading-api-frontend/src/app/algorithm-configs.ts
+++ apps/large-trading-api-frontend/src/app/algorithm-configs.ts
@@ -1,15 +1,60 @@
 import { Algorithm, AlgorithmConfig } from '../../../../libs/shared/src/lib/interfaces';
 
 export const AlgorithmConfigs: Partial<Record<Algorithm, AlgorithmConfig>> = {
-  [Algorithm.Momentum]: { single: { streak: 5 } },
-  [Algorithm.Macd]: { single: { fast: 12, slow: 26, signal: 9 } },
-  [Algorithm.Rsi]: { single: { length: 7 } },
-  [Algorithm.Ema]: { single: { periodOpen: 80, periodClose: 80 } },
-  [Algorithm.Bb]: { single: { period: 21 } },
-  [Algorithm.DeepTrend]: { single: {} },
-  [Algorithm.Dca]: { single: {} },
-  [Algorithm.MeanReversion]: { single: { threshold: 0.15, profitBasedTrailingStopLoss: 0.3, startStreak: 0 } },
-  [Algorithm.TrendLine]: { single: { percentProfit: 0.3 } },
-  [Algorithm.MarketStructure]: { single: { space: 5 } },
-  [Algorithm.Example]: { single: { size: 10 } },
+  [Algorithm.Momentum]: {
+    single: {
+      streak: 5
+    }
+  },
+  [Algorithm.Macd]: {
+    single: {
+      fast: 12,
+      slow: 26,
+      signal: 9
+    }
+  },
+  [Algorithm.Rsi]: {
+    single: {
+      length: 7
+    }
+  },
+  [Algorithm.Ema]: {
+    single: {
+      periodOpen: 80,
+      periodClose: 80
+    }
+  },
+  [Algorithm.Bb]: {
+    single: {
+      period: 21
+    }
+  },
+  [Algorithm.DeepTrend]: {
+    single: {}
+  },
+  [Algorithm.Dca]: {
+    single: {}
+  },
+  [Algorithm.MeanReversion]: {
+    single: {
+      threshold: 0.15,
+      profitBasedTrailingStopLoss: 0.3,
+      startStreak: 0
+    }
+  },
+  [Algorithm.TrendLine]: {
+    single: {
+      percentProfit: 0.3
+    }
+  },
+  [Algorithm.MarketStructure]: {
+    single: {
+      space: 5
+    }
+  },
+  [Algorithm.Example]: {
+    single: {
+      size: 10
+    }
+  },
 };
